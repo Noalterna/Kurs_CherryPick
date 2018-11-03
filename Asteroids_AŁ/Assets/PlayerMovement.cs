684938b4afca3ba51f5480f0f4ea71c8e3fb0a79
Noalterna <noalterna@gmail.com>
2018-11-03 18:15:21 +0100

    Player porusza sie gora-dol i na boki

---
+++ Asteroids_AŁ/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Vector2 position;
+    public int speed = 6;
+    float horizontalMove;
+    float verticalMove;
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove += Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        //transform.position = new Vector2(horizontalMove,0);
+
+        verticalMove += Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        transform.position = new Vector2(horizontalMove, verticalMove);
+
+	}
+}
