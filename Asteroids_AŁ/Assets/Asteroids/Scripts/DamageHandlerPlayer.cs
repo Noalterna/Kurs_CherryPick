419ae1115e02a6a0e785bf7bf82f98cae9cce7fa
Noalterna <noalterna@gmail.com>
2018-11-06 20:00:35 +0100

    poruszanie asteroid i poprawki

---
+++ Asteroids_AŁ/Assets/Asteroids/Scripts/DamageHandlerPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandlerPlayer : MonoBehaviour {
+
+    int health = 1;
+
+    //if player hits the enemy he dies
+    private void OnTriggerEnter2D()
+    {
+        health--;
+    }
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        GameController.instance.PlayerDied();
+    }
+}
---
+++ Asteroids_AŁ/Assets/Asteroids/Scripts/HexagonMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonMovement : MonoBehaviour {
+
+    public float speed = 2f;
+    Vector2 pos;
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D r2d = GetComponent<Rigidbody2D>();
+        r2d.angularVelocity = Random.Range(-20, 20);
+    }
+	// Update is called once per frame
+	void Update () {
+        pos = transform.position;
+
+        if (pos.x >= 20 || pos.y >=20 || pos.x <=-20 || pos.x <=-20)
+        {
+            speed *= -1;
+        }
+        transform.Translate(speed*Time.deltaTime , speed * Time.deltaTime ,0);
+	}
+}
