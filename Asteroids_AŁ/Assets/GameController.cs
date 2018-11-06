65b16874cf2d160c9b836544c9aa50c467296dfb
Noalterna <noalterna@gmail.com>
2018-11-06 12:56:46 +0100

    Spawnowanie enemies

---
+++ Asteroids_AŁ/Assets/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+    public Text scoreText;
+    public GameObject gameOvertext;
+
+    private int score = 0;
+    public bool gameOver = false;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+    public void PlayerScored()
+    {
+        if (gameOver) return;
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+    public void PlayerDied()
+    {
+        gameOvertext.SetActive(true);
+        gameOver = true;
+    }
+}
